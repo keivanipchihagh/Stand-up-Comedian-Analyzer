8917340e7a8e0d9cd49438b30fcaa37c48245b78
keivanipchihagh <ipchi1380@gmail.com>
2021-05-05 12:44:18 +0430

    Saved scraped data

--- .virtual_documents/Web Scraping.ipynb.py
+++ .virtual_documents/Web Scraping.ipynb.py
@@ -1,5 +1,5 @@
 # Transcripts URLs
-transcript_urls = [
+urls = [
     'https://scrapsfromtheloft.com/2017/05/06/louis-ck-oh-my-god-full-transcript/',
     'https://scrapsfromtheloft.com/2017/04/11/dave-chappelle-age-spin-2017-full-transcript/',
     'https://scrapsfromtheloft.com/2018/03/15/ricky-gervais-humanity-transcript/',
@@ -19,32 +19,40 @@ transcript_urls = [
 from bs4 import BeautifulSoup
 import requests
 
-def get_content(transcript_url):
+def get_transcript(url):
     ''' Get paragraph contents of the URL in the spesified Div element'''
     
-    page = requests.get(transcript_url).text
+    page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     texts = [p.text for p in soup.find(class_ = 'elementor-widget-theme-post-content').find_all('p')]
     
-    print('Got contents for "{0}"'.format(transcript_url))
+    print('Got contents for "{0}"'.format(url))
+    return texts
 
 # Get transcrpts for all the URLs
-transcripts = []
-for url in transcript_urls:
-    transcript = get_content(url)
-    transcripts.append(transcript)
+transcripts = [get_transcript(url) for url in urls]
+print('Scraping completed.')
+
+names = [
+    'Lousic C.K.',
+    'Dave Chappelle',
+    'Ricky Gervais',
+    'Bo Burham',
+    'Bill Burr',
+    'Jim Jefferies',
+    'John Mulaney',
+    'Hasan Minhaj',
+    'Ali Wong',
+    'Anthony Jeselnik',
+    'Mike Birbiglia',
+    'Joe Rogan'
+]
 
 
+# Imports
+import pickle
 
+all_data = dict(zip(names, transcripts))
 
-
-
-
-
-
-
-
-
-
-
-
+with open('saves/transcripts_data.json', 'wb') as file:
+    pickle.dump(all_data, file)
